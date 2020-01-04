fbb0f81f0e2b99d2d9f89c78bac42aef44e50021
Niroj Pokhrel <nirojpokhrel@gmail.com>
2020-01-04 12:41:24 +0200

    Added game_layout from gimp

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "definitions.h"
+#include "extensions/cocos-ext.h"
+#include "ui/CocosGUI.h"
 #include <utility>
 
-class GameScene : public cocos2d::Layer {
+namespace cocoui = cocos2d::ui;
+class GameScene : public cocos2d::Layer, public cocos2d::ui::EditBoxDelegate {
 public:
   static cocos2d::Scene *createScene();
 
@@ -11,6 +15,15 @@ public:
 
   // implement the "static create()" method manually
   CREATE_FUNC(GameScene);
+
+  // override from cocos2d::EditBoxDelegate
+  void editBoxEditingDidBegin(cocos2d::ui::EditBox *editBox) override;
+  void editBoxEditingDidEndWithAction(
+      cocos2d::ui::EditBox *editBox,
+      cocos2d::ui::EditBoxDelegate::EditBoxEndAction action) override;
+  void editBoxTextChanged(cocos2d::ui::EditBox *editBox,
+                          const std::string &text) override;
+  void editBoxReturn(cocos2d::ui::EditBox *editBox) override;
 
 private:
   void Create3x3Matrix(const std::pair<uint32_t, uint32_t> &origin,
