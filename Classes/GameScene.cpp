88352d23299cb80511cd2f0f128a9e66bf6bd60f
Niroj Pokhrel <nirojpokhrel@gmail.com>
2020-01-04 12:41:24 +0200

    Added game_layout from gimp

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,6 +1,5 @@
 #include "GameScene.h"
-#include "extensions/cocos-ext.h"
-#include "ui/CocosGUI.h"
+#include <iostream>
 
 USING_NS_CC;
 
@@ -25,31 +24,7 @@ bool GameScene::init() {
 
   auto visibleSize = Director::getInstance()->getVisibleSize();
   Vec2 origin = Director::getInstance()->getVisibleOrigin();
-#if 0
-  ui::EditBox *editName = ui::EditBox::create(
-      Size(200, 50), ui::Scale9Sprite::create("res/selected_edit_box.png"));
-  editName->setPosition(Vec2(visibleSize.width / 2 + origin.x,
-                             visibleSize.height / 2 + origin.y));
-  editName->setFontSize(15);
-  editName->setFontColor(Color3B::RED);
-  editName->setPlaceHolder("Name:");
-  editName->setPlaceholderFontColor(Color3B::WHITE);
-  editName->setMaxLength(8);
-  editName->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
-
-  this->addChild(editName);
-#endif
-#if 0
-  auto start_pos = std::pair<uint32_t, uint32_t>{origin.x + 50, origin.y + 150};
-  uint32_t size = 3 * 60 + 4 * 5;
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      Create3x3Matrix({start_pos.first + j * size, start_pos.second + i * size},
-                      60, 5);
-    }
-  }
-
-#endif
+  // set background
   ui::ImageView *image_view = ui::ImageView::create("res/game_layout.png");
   auto scale_x = visibleSize.width / image_view->getContentSize().width;
   auto scale_y = visibleSize.height / image_view->getContentSize().height;
@@ -57,30 +32,76 @@ bool GameScene::init() {
   image_view->setPosition(Vec2(origin.x + visibleSize.width / 2,
                                origin.y + visibleSize.height / 2));
   this->addChild(image_view);
-
+  // edit box test
+  // starting position 91x91
+  // width is 87
+  // border width is 4
+  // 89 horizontal
+  int x_pos = constants::kBkLeftSeperation + constants::kBkCellBorder +
+              constants::kBkCellSize / 2;
+  int y_pos = constants::kBkImgHeight - constants::kBkTopSeperation -
+              constants::kBkCellBorder - constants::kBkCellSize / 2;
+  auto start_pos = std::pair<uint32_t, uint32_t>{x_pos, y_pos};
+  uint32_t size = 3 * (constants::kBkCellSize + constants::kBkCellBorder);
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      Create3x3Matrix({start_pos.first + j * size, start_pos.second - i * size},
+                      constants::kBkCellSize, constants::kBkCellBorder);
+    }
+  }
   return true;
 }
 
 void GameScene::Create3x3Matrix(const std::pair<uint32_t, uint32_t> &origin,
                                 uint32_t cell_size, uint32_t cell_diff) {
+  auto visibleSize = Director::getInstance()->getVisibleSize();
+  auto x_scale = visibleSize.width / constants::kBkImgWidth;
+  auto y_scale = visibleSize.height / constants::kBkImgHeight;
   std::pair<uint32_t, uint32_t> current_pos = {0, 0};
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       ui::EditBox *editName = ui::EditBox::create(
-          Size(cell_size, cell_size),
-          ui::Scale9Sprite::create("res/selected_edit_box.png"));
+          Size(cell_size * x_scale, cell_size * y_scale),
+          ui::Scale9Sprite::create("res/wrong_edit_box.png"));
       current_pos.first = origin.first + i * (cell_size + cell_diff);
-      current_pos.second = origin.second + j * (cell_size + cell_diff);
-      editName->setPosition(Vec2(current_pos.first, current_pos.second));
+      current_pos.second = origin.second - j * (cell_size + cell_diff);
+      editName->setPosition(
+          Vec2(x_scale * current_pos.first, y_scale * current_pos.second));
       editName->setFontSize(50);
       editName->setFontColor(Color3B::RED);
-      editName->setPlaceholderFontColor(Color3B::WHITE);
+      editName->setOpacity(0);
       editName->setMaxLength(1);
       editName->setInputMode(ui::EditBox::InputMode::NUMERIC);
       editName->setTextHorizontalAlignment(cocos2d::TextHAlignment::CENTER);
       editName->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
+      editName->setDelegate(this);
 
       this->addChild(editName);
     }
+  }
+}
+
+void GameScene::editBoxEditingDidBegin(cocos2d::ui::EditBox *editBox) {
+  editBox->setOpacity(100);
+}
+
+void GameScene::editBoxEditingDidEndWithAction(
+    cocos2d::ui::EditBox *editBox,
+    cocos2d::ui::EditBoxDelegate::EditBoxEndAction action) {}
+
+void GameScene::editBoxTextChanged(cocos2d::ui::EditBox *editBox,
+                                   const std::string &text) {}
+
+void GameScene::editBoxReturn(cocos2d::ui::EditBox *editBox) {
+  auto text = editBox->getText();
+  if (std::strlen(text) > 2) {
+    // usually 1 will be the element and additional null termination '/0'
+    editBox->setOpacity(100);
+    editBox->setText("?");
+  } else if (editBox->getMaxLength() == 1 &&
+             std::isalpha(*editBox->getText())) {
+    editBox->setOpacity(100);
+  } else {
+    editBox->setOpacity(0);
   }
 }
---
+++ Classes/definitions.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace constants {
+// background image information
+constexpr uint16_t kBkImgWidth = 1920;
+constexpr uint16_t kBkImgHeight = 1080;
+constexpr uint16_t kBkCellSize = 87;
+constexpr uint16_t kBkCellBorder = 4;
+constexpr uint16_t kBkTopSeperation = 91;
+constexpr uint16_t kBkLeftSeperation = 89;
+} // namespace constants
