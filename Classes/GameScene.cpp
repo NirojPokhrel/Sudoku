d7554efc19c3b35e26d9f1145169a7ec42187af9
Niroj Pokhrel <nirojpokhrel@gmail.com>
2020-01-05 12:35:45 +0200

    Added unittests

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -43,44 +43,51 @@ bool GameScene::init() {
               constants::kBkCellBorder - constants::kBkCellSize / 2;
   auto start_pos = std::pair<uint32_t, uint32_t>{x_pos, y_pos};
   uint32_t size = 3 * (constants::kBkCellSize + constants::kBkCellBorder);
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      Create3x3Matrix({start_pos.first + j * size, start_pos.second - i * size},
-                      i, j);
-    }
-  }
+  Create3x3Matrix({start_pos.first, start_pos.second});
   return true;
 }
 
-void GameScene::Create3x3Matrix(const std::pair<uint32_t, uint32_t> &origin,
-                                uint32_t box_i, uint32_t box_j) {
+void GameScene::Create3x3Matrix(const std::pair<uint32_t, uint32_t> &origin) {
   auto visibleSize = Director::getInstance()->getVisibleSize();
   auto x_scale = visibleSize.width / constants::kBkImgWidth;
   auto y_scale = visibleSize.height / constants::kBkImgHeight;
   std::pair<uint32_t, uint32_t> current_pos = {0, 0};
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      ui::EditBox *editName = ui::EditBox::create(
-          Size(constants::kBkCellSize * x_scale,
-               constants::kBkCellSize * y_scale),
-          ui::Scale9Sprite::create("res/wrong_edit_box.png"));
-      current_pos.first = origin.first + i * (constants::kBkCellSize +
-                                              constants::kBkCellBorder);
-      current_pos.second = origin.second - j * (constants::kBkCellSize +
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (!game_state_[j][i]) {
+        ui::EditBox *editName = ui::EditBox::create(
+            Size(constants::kBkCellSize * x_scale,
+                 constants::kBkCellSize * y_scale),
+            ui::Scale9Sprite::create("res/wrong_edit_box.png"));
+        current_pos.first = origin.first + i * (constants::kBkCellSize +
                                                 constants::kBkCellBorder);
-      editName->setPosition(
-          Vec2(x_scale * current_pos.first, y_scale * current_pos.second));
-      editName->setFontSize(50);
-      editName->setFontColor(Color3B::RED);
-      editName->setOpacity(0);
-      editName->setMaxLength(1);
-      editName->setInputMode(ui::EditBox::InputMode::NUMERIC);
-      editName->setTextHorizontalAlignment(cocos2d::TextHAlignment::CENTER);
-      editName->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
-      editName->setDelegate(this);
-      editName->setActionTag(9 * (j + 3 * box_i) + (3 * box_j) + i + 1);
-
-      this->addChild(editName);
+        current_pos.second = origin.second - j * (constants::kBkCellSize +
+                                                  constants::kBkCellBorder);
+        editName->setPosition(
+            Vec2(x_scale * current_pos.first, y_scale * current_pos.second));
+        editName->setFontSize(50);
+        editName->setFontColor(Color3B::RED);
+        editName->setOpacity(0);
+        editName->setMaxLength(1);
+        editName->setInputMode(ui::EditBox::InputMode::NUMERIC);
+        editName->setTextHorizontalAlignment(cocos2d::TextHAlignment::CENTER);
+        editName->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
+        editName->setDelegate(this);
+        editName->setActionTag(9 * j + i);
+        this->addChild(editName);
+      } else {
+        ui::TextField *textBox = ui::TextField::create();
+        current_pos.first = origin.first + i * (constants::kBkCellSize +
+                                                constants::kBkCellBorder);
+        current_pos.second = origin.second - j * (constants::kBkCellSize +
+                                                  constants::kBkCellBorder);
+        textBox->setPosition(
+            Vec2(x_scale * current_pos.first, y_scale * current_pos.second));
+        textBox->setFontSize(30);
+        textBox->setMaxLength(1);
+        textBox->setString(std::to_string(game_state_[j][i]));
+        this->addChild(textBox);
+      }
     }
   }
 }
@@ -100,13 +107,24 @@ void GameScene::editBoxReturn(cocos2d::ui::EditBox *editBox) {
   auto text = editBox->getText();
   if (std::strlen(text) > 2) {
     // usually 1 will be the element and additional null termination '/0'
-    editBox->setOpacity(100);
+    editBox->setOpacity(150);
     editBox->setText("?");
   } else if (editBox->getMaxLength() == 1 &&
              std::isalpha(*editBox->getText())) {
-    editBox->setOpacity(100);
+    editBox->setOpacity(150);
   } else {
-    editBox->setOpacity(0);
+    auto pos = game_util::ConvertIndexToPoint(editBox->getActionTag());
+    game_state_[pos.first][pos.second] = 0;
+    uint8_t value = std::stoi(editBox->getText());
+    if (!game_util::IsEntryValid(game_state_, pos, value)) {
+      editBox->setOpacity(150);
+    } else {
+      editBox->setOpacity(0);
+    }
+    game_state_[pos.first][pos.second] = value;
+    if (IsSpaceFilled(game_state_)) {
+      // game over
+    }
   }
   std::cout << editBox->getActionTag() << std::endl;
 }
--- Classes/game_logic/game_util.cpp
+++ Classes/game_logic/game_util.cpp
@@ -1,6 +1,8 @@
 #include "game_util.h"
+#include <iostream>
 
-game_util::game_board game_util::GetNewGame() {
+namespace game_util {
+game_board GetNewGame() {
   game_board brd = {{{5, 3, 0, 0, 7, 0, 0, 0, 0},
                      {6, 0, 0, 1, 9, 5, 0, 0, 0},
                      {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -10,4 +12,71 @@ game_util::game_board game_util::GetNewGame() {
                      {0, 6, 0, 0, 0, 0, 2, 8, 0},
                      {0, 0, 0, 4, 1, 9, 0, 0, 5},
                      {0, 0, 0, 0, 8, 0, 0, 7, 9}}};
+  return brd;
 }
+
+std::pair<uint8_t, uint8_t> ConvertIndexToPoint(uint8_t index) {
+  std::pair<uint8_t, uint8_t> point;
+  point.first = index / 9;
+  point.second = index % 9;
+
+  return point;
+}
+
+uint8_t ConvertPointToIndex(const std::pair<uint8_t, uint8_t> &point) {
+  return static_cast<uint8_t>(9 * point.first + point.second);
+}
+
+inline bool VerticalOk(const game_board &brd, const cpoint &entry,
+                       uint8_t value) {
+  for (uint8_t i = 0; i < 9; ++i) {
+    if (brd[entry.first][i] == value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+inline bool HorizontalOk(const game_board &brd, const cpoint &entry,
+                         uint8_t value) {
+  for (uint8_t i = 0; i < 9; ++i) {
+    if (brd[i][entry.second] == value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+inline bool BoxOk(const game_board &brd, const cpoint &entry, uint8_t value) {
+  uint8_t i = static_cast<uint8_t>(entry.first - entry.first % 3);
+  uint8_t j = static_cast<uint8_t>(entry.second - entry.second % 3);
+  uint8_t i_end = static_cast<uint8_t>(i + 3);
+  uint8_t j_end = static_cast<uint8_t>(j + 3);
+  for (; i < i_end; ++i) {
+    for (; j < j_end; ++j) {
+      if (brd[i][j] == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool IsEntryValid(const game_board &brd, const cpoint &entry, uint8_t value) {
+  // check the box i.e. 3x3 board
+  // check the vertical and horizontal region for overlapping
+  return VerticalOk(brd, entry, value) && HorizontalOk(brd, entry, value) &&
+         BoxOk(brd, entry, value);
+}
+
+bool IsSpaceFilled(const game_board &brd) {
+  for (uint8_t i = 0; i < 9; ++i) {
+    for (uint8_t j = 0; j < 9; ++j) {
+      if (!brd[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+} // namespace game_util
