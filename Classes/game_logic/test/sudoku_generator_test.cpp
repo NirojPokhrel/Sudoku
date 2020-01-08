a09b6fd7a3868fa8ff75d4667c08ad2a05eecba2
Niroj Pokhrel <nirojpokhrel@gmail.com>
2020-01-08 18:07:42 +0200

    Added unit test for solver and generator

--- Classes/game_logic/test/sudoku_generator_test.cpp
+++ Classes/game_logic/test/sudoku_generator_test.cpp
@@ -4,4 +4,21 @@
 TEST(SudokuGenerator, GenerateBoard) {
   game_logic::SudokuGenerator sudoku_gen;
   sudoku_gen.GenerateBoard(40);
+  auto CountEmptyElem = [](const game_util::game_board &brd) {
+    int counter = 0;
+    for (const auto &row : brd) {
+      for (auto elem : row) {
+        if (elem == 0) {
+          counter++;
+        }
+      }
+    }
+    return counter;
+  };
+  auto brd = sudoku_gen.GenerateBoard(40);
+  EXPECT_EQ(40, CountEmptyElem(brd));
+  brd = sudoku_gen.GenerateBoard(30);
+  EXPECT_EQ(30, CountEmptyElem(brd));
+  brd = sudoku_gen.GenerateBoard(50);
+  EXPECT_EQ(50, CountEmptyElem(brd));
 }
